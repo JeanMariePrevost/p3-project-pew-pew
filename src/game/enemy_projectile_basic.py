5e6941665313fbc41d02d7ef9124ba6983e7e314
JeanMariePrevost <jeanmarie.prevost@outlook.com>
2024-11-14 12:48:24 -0500

    Add  hit particle effect on enemy projectiles collisions

--- src/game/enemy_projectile_basic.py
+++ src/game/enemy_projectile_basic.py
@@ -1,9 +1,9 @@
-import pygame
-
+from animated_renderable import AnimatedRenderable
 from game.collision_type_set import CollisionType, CollisionTypeSet
 from game.projectile import Projectile
 import global_events
 import global_services
+from particle import Particle
 from renderable_flash_wrapper import RenderableFlashWrapper
 
 
@@ -20,8 +20,10 @@ class EnemyProjectileBasic(Projectile):
         self.set_collision_types(collision_class=CollisionType.ENEMY_SHOT, collision_targets=CollisionTypeSet(CollisionType.PLAYER))
 
     def on_collision_with_target(self, other):
-        self.destroy()
         if other == global_services.get_player():
             global_events.player_took_damage.trigger(self)
             RenderableFlashWrapper(global_services.get_player().renderable, (255, 0, 0), 0.4, 12)
-            # TODO: Implement player taking damage
+            animation = AnimatedRenderable("assets/Simple explosion", loop=False, ticks_per_frame=3, auto_tick=True)
+            hit_particle = Particle(self.rect.centerx, self.rect.centery, animation)
+            hit_particle.set_scale(0.5)
+        self.destroy()
